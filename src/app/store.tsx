f5080e8694dce76721c831eb3753ccf6b4197071
Seckin Ozturk <franseck102@gmail.com>
2024-12-07 20:34:45 -0500

    todoSlice setup

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { configureStore } from '@reduxjs/toolkit'
+import todoReducer from "../features/todoSlice"
 
-const store = () => {
-  return (
-    <div>store</div>
-  )
-}
+export const store = configureStore({
+  reducer: {
+    todo :todoReducer
+  },
+})
 
-export default store
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
