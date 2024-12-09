d8925f30b922039378e4bd560e3ebfa59adc6dc1
Seckin Ozturk <franseck102@gmail.com>
2024-12-08 21:37:05 -0500

    edit and remove

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -3,26 +3,38 @@ import { FaCheckCircle } from "react-icons/fa";
 import { GrEdit } from "react-icons/gr";
 import { TodoProps } from "../../types/AllTypes";
 import { useDispatch } from "react-redux";
-import { removeTodoById } from "../../features/todoSlice";
+import { removeTodoById, updateTodo } from "../../features/todoSlice";
 import { useState } from "react";
 
 const Todo = ({todoProps} : TodoProps) => {
   const {id, content} = todoProps;
 const dispatch = useDispatch();
 
-const [edit, setEdit]= useState<Boolean>(false);
+const [edit, setEdit]= useState<boolean>(false);
+const [newTodo, setNewTodo] = useState<string>(content);
 
 const handleRemoveTodo =()=>{
 dispatch(removeTodoById(id))
 };
 
+const handleUpdateTodo =()=>{
+  const payload = {
+    id:id,
+    content : newTodo
+  }
+  dispatch(updateTodo(payload))
+  setEdit(false)
+}
+
 
   return (
     <div className="todo">
-        <div>{content}</div>
+        {edit ? <input type="text" value={newTodo} onChange={(e: React.ChangeEvent<HTMLInputElement>)=> setNewTodo(e.target.value)} /> :
+        <div>{content}</div>}
+
 <div className="icons">
     <IoMdRemoveCircle onClick={handleRemoveTodo} />
-    {edit ? <FaCheckCircle/> :<GrEdit/> }
+    {edit ? <FaCheckCircle  onClick={handleUpdateTodo}/> :<GrEdit onClick={()=>setEdit(true)}/> }
     
 
 </div>
