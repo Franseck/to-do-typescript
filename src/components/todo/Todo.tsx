3cb3169833eadff80d19f18b51100c0d977e87c1
Seckin Ozturk <franseck102@gmail.com>
2024-12-08 21:15:00 -0500

    edit to do

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -2,15 +2,28 @@ import { IoMdRemoveCircle } from "react-icons/io";
 import { FaCheckCircle } from "react-icons/fa";
 import { GrEdit } from "react-icons/gr";
 import { TodoProps } from "../../types/AllTypes";
+import { useDispatch } from "react-redux";
+import { removeTodoById } from "../../features/todoSlice";
+import { useState } from "react";
 
 const Todo = ({todoProps} : TodoProps) => {
   const {id, content} = todoProps;
+const dispatch = useDispatch();
+
+const [edit, setEdit]= useState<Boolean>(false);
+
+const handleRemoveTodo =()=>{
+dispatch(removeTodoById(id))
+};
+
+
   return (
     <div className="todo">
         <div>{content}</div>
 <div className="icons">
-    <IoMdRemoveCircle/>
-    <GrEdit/>
+    <IoMdRemoveCircle onClick={handleRemoveTodo} />
+    {edit ? <FaCheckCircle/> :<GrEdit/> }
+    
 
 </div>
 
